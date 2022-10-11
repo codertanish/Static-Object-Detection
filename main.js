8a0820de7a08bd7dcafab52360ad4df75f661340
codertanish <81834790+codertanish@users.noreply.github.com>
2022-10-11 19:40:25 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+img = "";
+Status = "";
+objects = [];
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+
+function setup() {
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if(error) {
+        console.error();
+    }
+
+    console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+
+    if( Status != "") {
+        for(var i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Objects Detected";
+            percent = floor(objects[i].confidence * 100);
+            noStroke();
+            fill("#FF0000");
+            textFont("Comic Sans MS");
+            textSize(20);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 10, objects[i].y + 20);
+            stroke("#FF0000");
+            noFill();
+            strokeWeight(2);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+        }
+    }
+}
